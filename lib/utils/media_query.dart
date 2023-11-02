fac7514407c695fd62bbea66050305c01989c5be
Furqan Uddin <ffurqanuddin@gmail.com>
2023-11-03 03:15:38 +0500

    Auth UI Created (TABLET, DESKTOP VIEW) Responsive

---
+++ lib/utils/media_query.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class MQ{
+ static width (context)=> MediaQuery.sizeOf(context).width;
+  static height (context)=> MediaQuery.sizeOf(context).height;
+}
